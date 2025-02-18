b3b39d98d313070db57cb488d091c17d5edc0ea2
nag2548 <nadine.grabmair@gmx.de>
2025-02-18 23:22:30 +0100

    ⚗️ add some ui components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,10 @@
-import { useState } from 'react';
 import './App.css';
+import Page from '@/dashboard/Page.tsx';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div className="text-gray-500 dark:text-gray-400">
-				<h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-					Vite + React
-				</h1>
-				<div className="py-3">
-					<button
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
-						onClick={() => setCount(count => count + 1)}
-					>
-						count is {count}
-					</button>
-				</div>
-			</div>
+			<Page />
 		</>
 	);
 }
